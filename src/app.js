30fb7a714c52fafa9b85c08611406dce23aa4d1f
Baines Deal <bainesdeal@gmail.com>
2022-08-03 14:26:53 -0500

    added the projects section and button integration

--- src/app.js
+++ src/app.js
@@ -6,3 +6,62 @@ function toggleButton() {
 }
 
 hamburgerButton.addEventListener('click', toggleButton)
+
+let slideIndex = 0;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+const zoomBtn = document.querySelectorAll('.zoom-text')
+const allImages = document.querySelectorAll('.project-image');
+const imageView = document.querySelector('.image-view');
+const nextBtn = document.getElementById('.next-btn');
+const prevBtn = document.getElementById('prev-btn');
+const imageBox = document.querySelector('.image-box');
+
+let currentImageIndex = 0;
+
+imageView.addEventListener('click', function(){
+    this.style.display = "none";
+    imageBox.style.display = "none";
+
+})
+
+zoomBtn.forEach(function(btn, index){
+    btn.addEventListener('click', function(){
+        imageView.style.display = "block";
+        imageBox.style.display = "block";
+        currentImageIndex = index + 1;
+        currentImageDisplay(currentImageIndex);
+    })
+}
+)
+
+function currentImageDisplay(position){
+    imageBox.style.background = `url (src/images/ProjOne${currentImageIndex}.PNG)
+     center/cover no-repeat`;
+}
